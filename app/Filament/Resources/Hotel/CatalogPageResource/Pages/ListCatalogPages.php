23bc14764a168c9c64e13e2f3ea695220fac3a61
Airilxx <13097983+airilx96@users.noreply.github.com>
2024-12-30 14:38:43 +0800

    feat<hk>: add initial support for RCON actions

--- app/Filament/Resources/Hotel/CatalogPageResource/Pages/ListCatalogPages.php
+++ app/Filament/Resources/Hotel/CatalogPageResource/Pages/ListCatalogPages.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\Hotel\CatalogPageResource\Pages;
 
 use App\Filament\Resources\Hotel\CatalogPageResource;
+use App\Services\RconService;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCatalogPages extends ListRecords
@@ -13,6 +16,31 @@ class ListCatalogPages extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('update-catalog')
+                ->label('Update catalog (RCON)')
+                ->color('danger')
+                ->action(function () {
+                    $rconService = app(RconService::class);
+
+                    if (!$rconService->isConnected()) {
+                        Notification::make()
+                            ->body(__('The RCON service is not connected.'))
+                            ->icon('heroicon-o-exclamation-circle')
+                            ->iconColor('danger')
+                            ->send();
+
+                        return;
+                    } else {
+                        $rconService->updateCatalog();
+
+                        Notification::make()
+                        ->body(__('The catalog has been updated!'))
+                        ->icon('heroicon-o-check-circle')
+                        ->iconColor('success')
+                        ->send();
+                    }
+                }),
+
             CreateAction::make(),
         ];
     }
--- app/Filament/Resources/Hotel/WordFilterResource/Pages/ManageWordFilters.php
+++ app/Filament/Resources/Hotel/WordFilterResource/Pages/ManageWordFilters.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\Hotel\WordFilterResource\Pages;
 
 use App\Filament\Resources\Hotel\WordFilterResource;
-use Filament\Pages\Actions;
+use App\Services\RconService;
+use Filament\Actions\Action;
+use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageWordFilters extends ManageRecords
@@ -13,7 +16,32 @@ class ManageWordFilters extends ManageRecords
     protected function getActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Action::make('update-wordfilter')
+                ->label('Update wordfilter (RCON)')
+                ->color('danger')
+                ->action(function () {
+                    $rconService = app(RconService::class);
+
+                    if (!$rconService->isConnected()) {
+                        Notification::make()
+                            ->body(__('The RCON service is not connected.'))
+                            ->icon('heroicon-o-exclamation-circle')
+                            ->iconColor('danger')
+                            ->send();
+
+                        return;
+                    } else {
+                        $rconService->updateWordFilter();
+
+                        Notification::make()
+                        ->body(__('The wordfilter has been updated!'))
+                        ->icon('heroicon-o-check-circle')
+                        ->iconColor('success')
+                        ->send();
+                    }
+                }),
+
+            CreateAction::make(),
         ];
     }
 }
