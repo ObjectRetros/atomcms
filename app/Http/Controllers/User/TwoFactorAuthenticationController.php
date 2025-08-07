f538497a2b8799130c37d7d167daba6af2d36546
duckietm <troop@mailtjes.com>
2025-08-07 10:46:34 +0200

    :up: Small fix to route for 2FA

--- app/Http/Controllers/User/TwoFactorAuthenticationController.php
+++ app/Http/Controllers/User/TwoFactorAuthenticationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Laravel\Fortify\Actions\EnableTwoFactorAuthentication;
+use Laravel\Fortify\Actions\DisableTwoFactorAuthentication;
 
 class TwoFactorAuthenticationController extends Controller
 {
@@ -27,5 +28,11 @@ class TwoFactorAuthenticationController extends Controller
             return back()->withErrors('Invalid Two Factor Authentication code');
         }
         return redirect()->route('settings.two-factor')->with('success', __('Two-factor authentication has been confirmed.'));
+    }
+
+    public function destroy(Request $request, DisableTwoFactorAuthentication $disable): RedirectResponse
+    {
+        $disable($request->user());
+        return redirect()->route('settings.two-factor')->with('success', __('Two-factor authentication has been disabled.'));
     }
 }
